4318716ee56bf05e0ecb2734623ab4765a58af04
zdevt <zhangtao.hawk@qq.com>
2017-11-13 17:07:12 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
-sudo rm -rf ~/.vim*
+#sudo rm -rf ~/.vim*
 
-sudo chmod +x astyle_sh
-sudo chmod +x astyle
+#sudo chmod +x astyle_sh
+#sudo chmod +x astyle
 #sudo cp astyle /bin
 #sudo cp astyle_sh /bin
 
-sudo apt install build-essential cmake -y
-sudo apt install python-dev python3-dev -y
-sudo apt install ctags cscope -y
+#sudo apt install build-essential cmake -y
+#sudo apt install python-dev python3-dev -y
+#sudo apt install ctags cscope -y
 
-sudo ln -s $PWD/_vimrc ~/.vimrc
-sudo ln -s $PWD/_vim ~/.vim
+#sudo ln -s $PWD/_vimrc ~/.vimrc
+#sudo ln -s $PWD/_vim ~/.vim
 
 
+SYSTYPE=`uname`
+
+if [ $SYSTYPE = "Linux" ];
+then 
+  echo "Linux"
+elif [ $SYSTYPE = "Darwin" ];
+then 
+  echo "Mac"
+fi
 
