cff139391e54522180638957318cac8d6099ba35
zdevt <zhangtao.hawk@qq.com>
2018-02-08 09:04:55 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -6,22 +6,22 @@ SYSTYPE=`uname`
 #sudo apt install python-dev python3-dev -y
 #sudo apt install ctags cscope -y
 
-sudo rm -rf ~/.vim*
-sudo ln -s $PWD/_vimrc ~/.vimrc
-sudo ln -s $PWD/_vim ~/.vim
+rm -rf ~/.vim*
+ln -s $PWD/_vimrc ~/.vimrc
+ln -s $PWD/_vim ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-sudo chmod +x astyle_sh
-sudo \cp ./astyle_sh ~/.vim/astyle_sh
+chmod +x astyle_sh
+\cp ./astyle_sh ~/.vim/astyle_sh
 
 if [ $SYSTYPE = "Linux" ];
 then 
-  sudo chmod +x astyle
-  sudo \cp ./astyle ~/.vim/astyle
+  chmod +x astyle
+  \cp ./astyle ~/.vim/astyle
 elif [ $SYSTYPE = "Darwin" ];
 then 
-  sudo chmod +x astyle_mac
-  sudo \cp ./astyle_mac ~/.vim/astyle
+  chmod +x astyle_mac
+  \cp ./astyle_mac ~/.vim/astyle
 else 
   echo "Linux"
 fi
